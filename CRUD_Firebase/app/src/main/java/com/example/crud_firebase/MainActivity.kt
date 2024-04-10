3a6f98f42fceb8cd3962e4eb92b4207f0586d00d
Diego Rocha <drochaesc@gmail.com>
2024-04-10 13:43:53 -0600

    Parcial 2: CRUD Automoviles con Firebase

---
+++ CRUD_Firebase/app/src/main/java/com/example/crud_firebase/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.crud_firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.crud_firebase.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var databaseReference: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.searchButton.setOnClickListener {
+            val searchVehicleNumber: String = binding.searcVehicleNumber.text.toString()
+            if (searchVehicleNumber.isNotEmpty()) {
+                readData(searchVehicleNumber)
+            } else {
+                Toast.makeText(this, "Please enter the vehicle number", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun readData(vehicleNumber: String) {
+        databaseReference = FirebaseDatabase.getInstance().getReference("Vehicle Information")
+        databaseReference.child(vehicleNumber).get().addOnSuccessListener {
+            if (it.exists()) {
+                val ownerName = it.child("ownerName").value
+                val vehicleBrand = it.child("vehicleBrand").value
+                val vehicleRTO = it.child("vehicleRTO").value
+                Toast.makeText(this, "Results Found", Toast.LENGTH_SHORT).show()
+                binding.searcVehicleNumber.text.clear()
+                binding.readOwnerName.text = ownerName.toString()
+                binding.readVehicleBrand.text = vehicleBrand.toString()
+                binding.readVehicleRTO.text = vehicleRTO.toString()
+            } else {
+                Toast.makeText(this, "Vehicle number doesn't exist", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
